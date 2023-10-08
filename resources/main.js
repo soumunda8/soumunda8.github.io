f76dd58e1f58c22d63d699d26d952491fef2d0e3
soumunda8 <soumunda8@naver.com>
2023-10-09 01:39:59 +0900

    header 반응형 완료

---
+++ resources/main.js
@@ -0,0 +1,30 @@
+$(function(){
+    let headerHeight = Math.ceil($(".header").height());
+    let screenHeight = $(window).height() - headerHeight;
+    $("#about").css("height", screenHeight);
+    $("#skill").css("height", screenHeight);
+    $("#project").css("min-height", screenHeight);
+    $("#career").css("min-height", screenHeight);
+
+    $("h1").click(function(){ 
+        $("html, body").stop().animate({scrollTop:0}, 1000);
+    });
+    $("p").click(function(){
+        let divHeight = $(this).data("area");
+        $("html, body").stop().animate({scrollTop:$('#' + divHeight).offset().top - headerHeight}, 1000);
+        $(".menu_area").removeClass("active");
+    });
+});
+
+function showMenu() {
+    let check = $(".menu_area").hasClass("active");
+    if(check) {
+        $(".menu_area").removeClass("active");
+    } else {
+        $(".menu_area").addClass("active");
+    }
+}
+
+function hideMenu() {
+    $(".menu_area").removeClass("active");
+}
